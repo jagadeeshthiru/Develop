c0d643a32f46152d0a3d6dfac14991e79b601bc9
JAGADEESH THIRUMAREDDY <103480543+jagadeeshthiru@users.noreply.github.com>
2025-07-30 15:46:42 +0530

    Add files via upload

---
+++ MilkProduction_Data/MilkProduction_Data/src/main/java/com/example/MilkProduction_Data/MilkProductionDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.MilkProduction_Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkProductionDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkProductionDataApplication.class, args);
+	}
+
+}
---
+++ MilkProduction_Data/MilkProduction_Data/src/test/java/com/example/MilkProduction_Data/MilkProductionDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MilkProduction_Data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkProductionDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
